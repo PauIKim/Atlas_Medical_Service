9004614c982cbfb860a2cecc9073d4911763f1fd
Pyoung Kang Kim <pkim23@masonlive.gmu.edu>
2016-09-28 05:21:36 -0400

    Migrated to Nodejs and deleted deprecated files and added email verification on register

---
+++ server.js
@@ -0,0 +1,41 @@
+/**
+ * Created by rusty on 9/28/16.
+ */
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+var verifier = require("email-verify");
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(express.static("public"));
+
+
+app.post("/verify", function(req, res){
+    // console.log(req.toString()); see the full toString of the map object
+    // console.log(req); see what the req map looks like
+    var isValid = true;
+    verifier.verify(req.body.email, function(err, info){
+        console.log("HUH");
+        if( err ){
+            console.log(err);
+            console.log("called error");
+            isValid = false;
+        }
+        else {
+            isValid = false;
+            console.log("Successes (T/F): " + info.success);
+            console.log("Info: " + info.info);
+        }
+    });
+    res.json({isValid: isValid});
+});
+
+
+var port = process.env.PORT || 4000;
+
+app.listen(port, function(){
+    console.log("Atlas Medical Service Listening on " + port + "!");
+});
+
